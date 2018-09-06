a7756239c5bdc7404c0f16ab2429ff923e233054
AllenMative <2piano9637@gmail.com>
2018-09-06 15:52:14 +0900

    幽遠な宴
    Titleの文字スピードを可変にしました。

--- Assets/Scene/title.cs
+++ Assets/Scene/title.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class title : MonoBehaviour {
 
+    public float title_speed = 0.05f;
+
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector3 (0, -10f, 0); //タイトルロゴの初期位置 座標(0,-10,0)
@@ -16,7 +18,7 @@ public class title : MonoBehaviour {
 			if (Input.anyKeyDown) SceneManager.LoadScene ("test"); //何らかのキーが押されたらシーンをtestに切り替える
 			
 		} else { //もしy座標が0より小さかったら...
-			transform.Translate (0, 0.05f, 0); //y軸を +0.05
+			transform.Translate (0, title_speed, 0); //y軸を +0.05
 			if (Input.anyKey) transform.position = new Vector3 (0, 0f, 0); //何らかのキーが押されたら座標を(0,0,0)に
 
 		}
